cc08428f179d71eb01c69efb3a204db492ab2606
Alexander Svobodov <31383841+sidecuter@users.noreply.github.com>
2024-12-20 22:16:43 +0300

    Fix

--- pylint.sh
+++ pylint.sh
@@ -1,19 +1,25 @@
 #/usr/bin/env bash
 
+echo "Executing linter"
 output=$(python3 -m pylint --errors-only app/app.py)
 status=0
 if [ -n "$output" ]; then
     status=1
+    echo "Linting failed"
 else
     status=0
+    echo "Linting success"
 fi
+echo "Executing bandit"
 output=$(python3 -m bandit -r app/. -lll -q)
 if [ $status -eq 1 ]; then
     exit $status
 fi
 if [ -n "$output" ]; then
+    echo "bandit failed"
     status=1
 else
+    echo "Bandit success"
     status=0
 fi
 exit $status
